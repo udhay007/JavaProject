f35aa38b3580b91030a384f84e710c50a056d3dc
udhay007 <udhaysmart8@gmail.com>
2022-01-01 15:59:48 +0530

    update my valuessss

--- src/org/test/javaproject.java
+++ src/org/test/javaproject.java
@@ -1,13 +1,6 @@
 package org.test;
 
-public class javaproject {
 
-	public void employee() {
-		System.out.println("employee");
-	}
-	public void employee1() {
-		System.out.println("employee1");
-	}
-	public static void main (args )
+	//public static void main (args )
 	
-}
+
